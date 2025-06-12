54d37d0137e0d52b0c1ca822e5f3334daf3a6f5f
Brisa <flores.brisacandela@iesconcepcion.edu.ar>
2025-06-12 20:25:10 -0300

    p1

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hola mundo")
+function actualizar(){
+    const valor = document.getElementById("texto").value
+    document.getElementById("input").addEventListener("input", function() {
+        document.getElementById("mH1").textContent = this.value;
+    });
+    
+}
